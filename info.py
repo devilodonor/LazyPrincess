b934ce2100d358485ec0d60867deb45424d326a5
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2024-03-28 08:37:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
 LZURL_PRIME_USERS = [int(lazyurlers) if id_pattern.search(lazyurlers) else lazyurlers for lazyurlers in environ.get('LZURL_PRIME_USERS', '5546051083 5256724194').split()]
 lazy_groups = environ.get('LAZY_GROUPS','')
 LAZY_GROUPS = [int(lazy_groups) for lazy_groups in lazy_groups.split()] if lazy_groups else None # ADD GROUP ID IN THIS VARIABLE
-my_users = [int(my_users) if id_pattern.search(my_users) else my_users for my_users in environ.get('MY_USERS', '').split()]
+my_users = [int(my_users) if id_pattern.search(my_users) else my_users for my_users in environ.get('MY_USERS', '0').split()]
 MY_USERS = (my_users) if my_users else []
 
 # Online Stream and Download
